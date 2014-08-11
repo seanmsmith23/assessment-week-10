bbb55850bcd9ff2e428f33e600b7316d51204165
Sean Smith <seansmith1020@gmail.com>
2014-08-11 09:15:40 -0600

    anon user can view the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  resources :about, only: :index
 end
--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -1,9 +1,20 @@
 require "rails_helper"
 
-feature "Applcation" do
+feature "Application" do
   scenario "Can do stuff" do
     visit "/"
 
     expect(page).to have_content "Welcome"
+  end
+
+  scenario "Anon users can visit an about page" do
+    visit "/"
+
+    expect(page).to have_link("About")
+
+    click_link("About")
+
+    expect(page).to have_content("About")
+    expect(page).to have_content("Not much to say here.")
   end
 end
